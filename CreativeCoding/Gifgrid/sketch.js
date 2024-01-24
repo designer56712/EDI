8a6c0f66d3cda3f51beb6fc32b00676ef4c7c05b
designer56712 <144836338+designer56712@users.noreply.github.com>
2024-01-24 11:56:33 -0500

    h

--- CreativeCoding/Gifgrid/sketch.js
+++ CreativeCoding/Gifgrid/sketch.js
@@ -2,22 +2,28 @@
 
 let gif;
 
-function preLoad (assets/Meteor.gif)
+function preLoad () { 
+gif = loadImage (' assets/Meteor.gif ');
 
+}
 function setup() {
   createCanvas(windowWidth, windowHeight); 
-  strokeWeight(8);
-
-  fill('green');  
+  fill('lightgrey');
+  stroke ('white');
+  strokeWeight (1);
+ 
 }
   
 function draw() {
 
-  var num = 8
+background ('255,255,255  ');  
   
-  var sideLen = windowWidth
+  
+var num = 8
+  
+var sideLen = windowWidth
 
-  var sideLen = windowWidth/num;
+var sideLen = windowWidth/num;
   
   
     for (var y = 0; y < windowHeight; y = y + sideLen) {
@@ -25,11 +31,12 @@ function draw() {
     
     for (var x = 0; x < windowWidth; x = x + sideLen) {
  image (gif, x, y, sideLen,sideLen);
+}
     
-  }
-    }
 
 }
-function windowResized(){
+
+}
+function windowResized (){
   resizeCanvas (windowWidth, windowHeight);
 }
