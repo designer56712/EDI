545d4c2d79935cf91ef01d4a0a076626751fc486
designer56712 <144836338+designer56712@users.noreply.github.com>
2024-01-25 13:53:29 -0500

    h

--- CreativeCoding/emptyexample/sketch.js
+++ CreativeCoding/emptyexample/sketch.js
@@ -1,7 +1,32 @@
-function setup() {
- 
-}
+let gif;
 
-function draw() {
+function preload ()
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  strokeWeight(8);
+
+  fill('green');  
+}
   
+function draw() {
+
+  var num = 2
+  
+  var sideLen = windowWidth
+
+  var sideLen = windowWidth/num;
+  
+  
+    for (var y = 0; y < windowHeight; y = y + sideLen) {
+    
+    
+    for (var x = 0; x < windowWidth; x = x + sideLen) {
+ image (gif , x, y, sideLen, sideLen); 
+  }
+    }
+
+}
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight);
 }
