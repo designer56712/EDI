0340a21c195e7bd4d6fe0fdaa8ad1cd46a9b40a7
designer56712 <144836338+designer56712@users.noreply.github.com>
2024-01-30 13:07:37 -0500

    g

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+
+let gif;
+
+function preLoad () { 
+gif = loadImage (' assets/meteor.gif');
+
+}
+function setup() {
+  createCanvas (windowWidth, windowHeight); 
+  fill('lightgrey');
+  stroke ('white');
+  strokeWeight (1);
+ 
+}
+  
+function draw() {
+
+background ('255,255,255  ');  
+  
+  
+var num = 8
+  
+var sideLen = windowWidth
+
+var sideLen = windowWidth/num;
+  
+  
+    for (var y = 0; y < windowHeight; y = y + sideLen) {
+    
+    
+    for (var x = 0; x < windowWidth; x = x + sideLen) {
+ image (gif, x, y, sideLen,sideLen);
+}
+    
+
+}
+
+}
+function windowResized (){
+  resizeCanvas (windowWidth, windowHeight);
+}
